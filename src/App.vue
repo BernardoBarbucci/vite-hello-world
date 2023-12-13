66f379f3102b12dc100e51744fb90bdb148c2001
“Bernardo <bernardobarbucci@gmail.com>
2023-12-13 19:30:21 +0100

    esercizio completato*

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,12 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <HelloWorld msg="Vite + Vue" />
   </div>
-  <Apptitle msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
+/* .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
@@ -28,5 +22,5 @@ import HelloWorld from './components/HelloWorld.vue'
 
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
-}
+} */
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,23 +10,29 @@ const count = ref(0)
 
 <template>
   <h1>La mia prima app con Vite!</h1>
-  <p>utilizzando Vue!</p>
+  <p><i class="fa-brands fa-galactic-republic"></i></p>
 </template>
 
 <style scoped>
 h1 {
-  transition: transform 0.5s ease-in-out;
+  transition: transform 1s ease-in-out;
+  filter: drop-shadow(0 0 2em #646cffaa);
+  color: #c2c5ffaa;
+  will-change: filter;
+  /* transition: filter 500ms; */
+
 }
 
 h1:hover {
   transform: scale(1.2);
   color: rgb(166 87 246);
+  filter: drop-shadow(0 0 2em rgb(166 87 246));
 }
 
 
 p {
   margin-top: 2rem;
-  font-size: 10px;
+  font-size: 2rem;
   transition: transform 0.5s ease-in-out;
 }
 
