6f8f699cb22fff3fb239aea9e49f48d975116cc3
“Bernardo <bernardobarbucci@gmail.com>
2023-12-13 18:38:27 +0100

    esercizio completato

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,32 +9,28 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <h1>La mia prima app con Vite!</h1>
+  <p>utilizzando Vue!</p>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+h1 {
+  transition: transform 0.5s ease-in-out;
+}
+
+h1:hover {
+  transform: scale(1.2);
+  color: rgb(166 87 246);
+}
+
+
+p {
+  margin-top: 2rem;
+  font-size: 10px;
+  transition: transform 0.5s ease-in-out;
+}
+
+p:hover {
+  transform: rotate(360deg);
 }
 </style>
